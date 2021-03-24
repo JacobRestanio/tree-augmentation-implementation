5f2a53cdfd2e6e7eb30cb82e14dfa0f4fa43e8b6
JacobRestanio <37187976+JacobRestanio@users.noreply.github.com>
2021-03-24 09:21:42 -0400

    Create starlike-tree-generator.c

---
+++ starlike-tree-generator.c
@@ -0,0 +1,1 @@
+//A starlike tree consists of a central vertex called root and several path graphs attached to it. More formally, a tree is starlike if it has exactly one vertex of degree greater than 2.
