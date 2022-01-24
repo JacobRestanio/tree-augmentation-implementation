e3fcca07ee3dcefc9d52398e2bc627db5ef859d6
CodyKlingler <cwklingler0413@gmail.com>
2022-01-24 00:24:01 -0500

    Added .h and .c files for nagamochi

---
+++ include/nagamochi.h
@@ -0,0 +1,35 @@
+#ifndef NAGAMOCHI_H
+#define NAGAMOCHI_H
+
+#include "graph.h"
+
+//
+
+int nagamochi(int size, graph* tree, double aproxFactor);
+
+//cond1()
+
+//cond2()
+
+//cond3()
+
+//cond4()
+
+//proc1()
+
+//proc2()
+
+//proc3()
+
+//proc4()
+
+//a3()
+
+//cover()
+
+
+// isLClosed()
+
+// leaf()
+
+#endif
---
+++ src/nagamochi.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../include/tree-helper.h"
+#include "../include/graph.h"
+
+int nagamochi(int size, graph* tree, double aproxFactor){
+    return 0;
+}
