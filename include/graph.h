e7eec16eafa87f8ed69a1960dcaa29e13684a982
Cody Klingler <cwklingler0413@gmail.com>
2022-01-28 13:37:15 -0500

    removed unused functions and files

--- include/graph.h
+++ include/graph.h
@@ -1,6 +1,35 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+
+/*
+typedef struct Edge {
+  int value;
+  int cost;
+  struct Edge* next;
+} edge;
+
+typedef struct Vertex {
+  int value;
+  int degree;
+  edge* nextEdge;
+  struct Vertex* nextVertex;
+} vertex;
+
+
+struct Graph {
+  int vertices;
+  int edges;
+  vertex* vertexSet;
+};
+
+typedef struct AdjList {
+   int value;
+   struct AdjList* next;
+} adjlist;
+*/
+
+
 /* the struct Edge:
    int v: the label of the destination vertex
    edge* next: a pointer to the next edge */
@@ -56,89 +85,6 @@ void vertex_set_visited(vertex* vs, int visited);
 void vertex_set_inpath(vertex* vs, int inpath);
 
 void vertex_set_minimum_incoming_edge(vertex* vs, int e);
-
-vertex* directedgraph_get_successors(directedgraph* dg);
-
-vertex* directedgraph_get_predecessors(directedgraph* dg);
-
-/* Allocates memory for and initializes a DirectedGraph struct
-   int v: the number of vertices in the directed graph
-   RETURNS: a directedgraph* with no edges */
-directedgraph* directedgraph_create(int v);
-
-/* Checks if an edge exists from source to destination in a directed graph
-   directedgraph* g: the directed graph to check for edge
-   int source: the source vertex
-   int destination: the destination vertex
-   RETURNS: returns 1 if the edge exists, 0 if not */
-int directedgraph_is_successor(directedgraph* g, int source, int destination);
-
-/* Checks if an edge exists to destination from source in a directed graph
-   directedgraph* g: the directed graph to check for edge
-   int destination: the destination vertex
-   int source: the source vertex
-   RETURNS: 1 if the edge exists, 0 if not */
-int directedgraph_is_predecessor(directedgraph* g, int destination, int source);
-
-/* Returns the cost of an edge if it exists
-   directedgraph* g: the directed graph to get edge cost
-   int source: the source vertex
-   int destination: the destination vertex
-   RETURNS: cost of the edge if it exists, -1 if not
-   NOTE: returning -1 isn't optimal, since sometimes we use negative edge costs */
-int directedgraph_get_edge_cost(directedgraph* g, int source, int destination);
-
-/* Finds the specified edge and sets its edge cost
-   directedgraph* g: the directed graph to get edge cost
-   int source: the source vertex
-   int destination: the destination vertex
-   int cost: the cost to set the edge
-   MODIFIES: edge in g to new cost if the edge exists */
-void directedgraph_set_edge_cost(directedgraph* g, int source, int destination, int cost);
-
-/* Adds an edge to a directed graph with unit cost
-   directedgraph* g: the directed graph to add the edge to
-   int source: the source vertex
-   int destination: the destination vertex
-   MODIFES: g by adding a new edge */
-void directedgraph_add_edge(directedgraph* g, int source, int destination);
-
-void directedgraph_add_vertex(directedgraph* g, int v);
-
-/* Adds an edge to a directed graph with cost
-   directedgraph* g: the directed graph to add the edge to
-   int source: the source vertex
-   int destination: the destination vertex
-   int cost: the cost of the edge
-   MODIFIES: g by adding a new edge with associated cost */
-void directedgraph_add_weighted_edge(directedgraph* g, int source, int destination, int cost);
-
-/* Removes an edge from a directed graph
-   directedgraph* g: the directed graph to remove the edge from
-   int source: the source vertex
-   int destination: the destination vertex
-   MODIFES: g by removing an edge */
-void directedgraph_remove_edge(directedgraph* g, int source, int destination);
-
-void directedgraph_remove_vertex(directedgraph* g, int v);
-
-void directedgraph_remove_predecessors(directedgraph* g, int destination);
-
-/* Prints a directed graph to console
-   directedgraph* g: the directed graph to be printed
-   NOTE: probably don't want to do this for large graphs */
-void directedgraph_print(directedgraph* g);
-
-void directedgraph_print_predecessors(directedgraph* g);
-
-/* Prints a directed graph to console including edge costs
-   directedgraph* g: the directed graph to be printed
-   NOTE: probably don't want to do this for large graphs */
-void directedgraph_print_weights(directedgraph* g);
-
-/* Frees the memory of a directed graph
-   directedgraph* g: the directed graph to be freed */
-void directedgraph_free(directedgraph* g);
 
 /* Allocates memory for and initializes a Graph struct
    int v: the number of vertices in the graph
