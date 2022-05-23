92f7418bd4e058672d54c8f652613ef299034b90
jacob <jacobstanio@gmail.com>
2022-05-23 12:11:38 -0400

    updated space profiling functions

--- src/space/space.sh
+++ src/space/space.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-valgrind --tool=massif --stacks=yes --time-unit=ms --detailed-freq=1 --max-snapshots=1000 --ignore-fn=fredrickson --ignore-fn=edmondsAlgorithm --ignore-fn=main --massif-out-file=space/greedy.100 ./tree-augmentor 100
-   
-   valgrind --tool=massif --stacks=yes --time-unit=ms --detailed-freq=1 --max-snapshots=1000 --ignore-fn=fredrickson --ignore-fn=edmondsAlgorithm --ignore-fn=main --massif-out-file=space/greedy.500 ./tree-augmentor 500
+for i in {1..10}; do
+   valgrind --tool=massif --stacks=yes --time-unit=ms --detailed-freq=1 --max-snapshots=1000 --ignore-fn=greedyHeuristic --ignore-fn=main --massif-out-file=space/fredrickson.100.$i ./tree-augmentor 100 1
 
-for i in {1..100}; do
-   valgrind --tool=massif --stacks=yes --time-unit=ms --detailed-freq=1 --max-snapshots=1000 --ignore-fn=greedyHeuristic --ignore-fn=main --massif-out-file=space/fredrickson.100.$i ./tree-augmentor 100
-   
-   valgrind --tool=massif --stacks=yes --time-unit=ms --detailed-freq=1 --max-snapshots=1000 --ignore-fn=greedyHeuristic --ignore-fn=main --massif-out-file=space/fredrickson.500.$i ./tree-augmentor 500
+   valgrind --tool=massif --stacks=yes --time-unit=ms --detailed-freq=1 --max-snapshots=1000 --ignore-fn=fredrickson --ignore-fn=edmondsAlgorithm --ignore-fn=main --massif-out-file=space/greedy.100.$i ./tree-augmentor 100 1
+
+done
+
+for i in {1..1}; do
+   valgrind --tool=massif --stacks=yes --time-unit=ms --detailed-freq=1 --max-snapshots=1000 --ignore-fn=greedyHeuristic --ignore-fn=main --massif-out-file=space/fredrickson.1000.$i ./tree-augmentor 1000 1
+
+   valgrind --tool=massif --stacks=yes --time-unit=ms --detailed-freq=1 --max-snapshots=1000 --ignore-fn=fredrickson --ignore-fn=edmondsAlgorithm --ignore-fn=main --massif-out-file=space/greedy.1000.$i ./tree-augmentor 1000 1
+
 done
